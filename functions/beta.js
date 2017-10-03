2c2ec53ff01a3c0ca55b7b2f1f1f6f3666845b83
Stephan Smith <stephan.smith.bc93@gmail.com>
2017-10-03 09:41:44 -0400

    Spaces

--- functions/beta.js
+++ functions/beta.js
@@ -6,21 +6,21 @@ const mc = new mcapi.Mailchimp(functions.config().services.mailchimp.apikey);
 // This action will reject a given request remove any transactions charges.
 module.exports = functions.database.ref('/emails/{id}/actions/subscribe').onWrite((event) => {
 
-    var action = event.data.val();
-    var db = admin.database();
+	var action = event.data.val();
+	var db = admin.database();
 
-    if (!action || action == 'failed' || action == 'completed') return;
+	if (!action || action == 'failed' || action == 'completed') return;
 
-    return db.ref(`/emails/${event.params.id}`).once('value').then(snap => {
-        var data = snap.val();
+	return db.ref(`/emails/${event.params.id}`).once('value').then(snap => {
+		var data = snap.val();
 
-				mc.lists.subscribe({id: '9909dc4664', email:{ email: data.email }}, function(data) {
-					console.log('Success', data);
-					return event.data.ref.set('completed');
-				}, reason => {
-					console.log(reason)
-					return event.data.ref.set('failed');
-				});
-    });
+		mc.lists.subscribe({id: '9909dc4664', email:{ email: data.email }}, function(data) {
+			console.log('Success', data);
+			return event.data.ref.set('completed');
+		}, reason => {
+			console.log(reason)
+			return event.data.ref.set('failed');
+		});
+	});
 
 });
