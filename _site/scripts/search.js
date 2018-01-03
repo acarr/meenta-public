347ed90633e53f91040be54798abbb7734278328
Stephan Smith <stephan.smith.bc93@gmail.com>
2018-01-03 08:37:44 -0500

    Added in more test data

--- _site/scripts/search.js
+++ _site/scripts/search.js
@@ -2,5 +2,10 @@
 function searchSubmit(el) {
 	event.preventDefault();
 	var input = $(el).find('input[name=q]');
-	window.location = 'https://app.meenta.io/search?q=' + input.val();
+
+	if (window.location.hostname === 'training.meenta.io') {
+		window.location = 'https://demo.meenta.io/search?q=' + input.val();
+	} else {
+		window.location = 'https://app.meenta.io/search?q=' + input.val();
+	}
 }
